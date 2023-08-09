35a9fefb94103050dd0cb4efa953a6d2219b3835
PandaMan16 <contact@pandatown.fr>
2023-08-09 15:34:56 +0200

    add mail

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 require_once './partsPhp/header.php';
 
 $session = new Session();
-$mail = new Mail();
+// $mail = new Mail();
 
-$mail->sendEmail('shaefferaelita@gmail.com','test','test')
+// $mail->sendEmail('shaefferaelita@gmail.com','test','test');
 
 
 ?>
