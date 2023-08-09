4dc12f014f85cc4dba0ecf7b538942bcf91eab22
PandaMan16 <contact@pandatown.fr>
2023-08-09 16:14:17 +0200

    add getrole

--- Class/Session.php
+++ Class/Session.php
@@ -9,13 +9,25 @@ class Session extends Bdd{
         if(isset($_SESSION['id'])){
             $req = $this->Connect()->prepare("SELECT * FROM identifiant WHERE Id_identifiant = ?");
             $req->execute(array($_SESSION['id']));
+            // $req->execute(array("1"));
             $this->user = $req->fetch();
-            $this->user = $_SESSION['id'];
+            // $this->user = $_SESSION['id'];
         }else{
             $this->user = null;
         }
     }
 
+    public function GetRole(){
+        $req_n = $this->Connect()->prepare("SELECT * from nounou WHERE Id_identifiant = ?");
+        $req_n->execute(array($this->user['Id_identifiant']));
+        if($req_n->rowCount() == 0){
+            $req_v = $this->Connect()->prepare("SELECT * from parrent WHERE Id_identifiant =?");
+            $req_v->execute(array($this->user['Id_identifiant']));
+            return "Parent";
+        }else{
+            return "Nounou";
+        }
+    }
     public function GetNom(){
         return $this->user['nom'];
     }
@@ -29,6 +41,15 @@ class Session extends Bdd{
         }else{
             return true;
         }
+    }
+
+    public function Logout(){
+        session_destroy();
+    }
+
+    public function Login($user){
+        $this->user = $user;
+        $_SESSION['id'] = $user['Id_identifiant'];
     }
 
 }
