8138954f493976c7094b485ddc9cb6aada4bb80e
PandaMan16 <contact@pandatown.fr>
2023-08-09 15:34:53 +0200

    add mail

--- Class/Mail.php
+++ Class/Mail.php
@@ -1,7 +1,28 @@
 <?php 
 
-class Mail extends Bdd{
-    
-    
 
+class Mail{
+    
+    private $to;
+    private $subject;
+    private $message;
+    private $headers;
+
+    public function __construct(){
+        $this->headers = "From: nounou@pandatown.fr\r\n";
+        $this->headers .= "Reply-To: contact@pandatown.fr\r\n";
+        $this->headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    }
+    public function sendEmail($to, $subject, $message) {
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->message = $message;
+        $mailSent = mail($this->to, $this->subject, $this->message, $this->headers);
+        
+        if ($mailSent) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
