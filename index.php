1f4b512bd5717d884265c08e94c6937874e44bcf
PandaMan16 <contact@pandatown.fr>
2023-08-10 15:37:14 +0200

    add favicon

--- index.php
+++ index.php
@@ -17,13 +17,15 @@ $session = new Session();
     <meta name="description" content="">
     <title>I'm IN</title>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-<link
+    <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
       rel="stylesheet"
       integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
       crossorigin="anonymous"
     />
     <link rel="stylesheet" href="/style/style.scss" />
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
 
 </head>
 
