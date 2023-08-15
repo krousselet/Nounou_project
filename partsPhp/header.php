f5a703da35067adae03a4a9d7ce842ad8c8a1f26
Kevin R <krous74@gmail.com>
2023-08-15 15:51:34 +0200

    Mise en place préliminaire de FullCalendar pour automatiser la mise en place du calendrier en fonction des préférences renseignées

--- partsPhp/header.php
+++ partsPhp/header.php
@@ -8,6 +8,7 @@
     <title>I'm IN</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.1/main.css" rel="stylesheet">
     <link rel="stylesheet" href="./style/style.scss" />
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
