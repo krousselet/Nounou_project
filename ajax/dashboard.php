3168438dcab16f5ae2d8d744828101177391c424
PandaMan16 <contact@pandatown.fr>
2023-08-18 16:16:50 +0200

    Payload

--- ajax/dashboard.php
+++ ajax/dashboard.php
@@ -1,9 +1,9 @@
 <?php
 include __DIR__.'/../partsPhp/loader.php';
-$session = new Session();
-if(!$session->isLogin()){
-    header('Location: ../index.php');
-}
+// $session = new Session();
+// if(!$session->isLogin()){
+//     header('Location: ../index.php');
+// }
 if(isset($_POST['action'])){
     switch($_POST['action']){
         case 'getCharges':
--- js/section/Dashboard.js
+++ js/section/Dashboard.js
@@ -3,18 +3,25 @@ import { panda } from 'https://pandatown.fr/lib/pandalib.php';
 const page = {
     init : function () {
                 document.querySelector('.btn_d_addChild').addEventListener('click', () => {
-          let form = panda.util.newelem('form',{className:""});
+          let form = panda.util.newelem('form',{className:"form_d_addChild"});
             form.appendChild(panda.util.newelem('input',{"type":"text","name":"Child_name","placeholder":"Nom de l'enfant"}));
-            form.appendChild(panda.util.newelem('input',{"type":"number","name":"Child_prenom","placeholder":"Prenom de l'enfant"}));
+            form.appendChild(panda.util.newelem('input',{"type":"text","name":"Child_prenom","placeholder":"Prenom de l'enfant"}));
             form.appendChild(panda.util.newelem('input',{"type":"number","name":"Child_age","placeholder":"Age de l'enfant"}));
-            let sendbtn = panda.util.newelem('input',{"type":"submit","name":"Add_Child"});
-            sendbtn.addEventListener('click', () => {
-              panda.ajax('./ajax/dashboard.php', {"action":"addCharges","Child_name":form.Child_name.value,"Child_prenom":form.Child_prenom.value,"Child_age":form.Child_age.value}, (data) => {
+            let sendbtn = panda.util.newelem('input',{"type":"submit","name":"Add_Child","className":"btn_d_addChildS"});
+            
+            form.appendChild(sendbtn);
+            document.querySelector('.blur').innerHTML = form.outerHTML;
+            document.querySelector('.form_d_addChild > .btn_d_addChildS').addEventListener('click', (e) => {
+              console.log(e);
+              e.preventDefault();
+              let forme = document.querySelector('.form_d_addChild');
+              const nom = forme.querySelector("input[name=Child_name]").value;
+              const prenom = forme.querySelector("input[name=Child_prenom]").value;
+              const age = forme.querySelector ("input[name=Child_age]").value;
+              panda.ajax('./ajax/dashboard.php', {"action":"addCharges","Child_name":nom,"Child_prenom":prenom,"Child_age":age}, (data) => {
                 console.log(data);
               });
             })
-            form.appendChild(sendbtn);
-            document.querySelector('.blur').innerHTML = form.outerHTML;
           // panda.ajax('./ajax/dashboard.php', {"action":"getCharges"} , (data) => {
             
           // });
