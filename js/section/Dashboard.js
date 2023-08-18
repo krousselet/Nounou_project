cbe637d1c3d92a1cf8fe659d05e805e75c18e57b
Kevin R <krous74@gmail.com>
2023-08-18 16:03:24 +0200

    direction to take

--- js/section/Dashboard.js
+++ js/section/Dashboard.js
@@ -1,24 +1,24 @@
 import { panda } from 'https://pandatown.fr/lib/pandalib.php';
 
 const page = {
-    init : function () {
-        const calendarEl = document.getElementById('calendar');
+  init: function () {
+    const calendarEl = document.getElementById('calendar');
 
-        const calendar = new FullCalendar.Calendar(calendarEl, {
-          //Représentation sur un mois//
-          initialView: 'dayGridMonth',
-          events: [
-            //objets pour peupler les cellules//
-            {
-        title: 'Jean Euze',
-        start: '2023-08-15T08:00:00',
-        end: '2023-08-15T09:30:00',
-      },
-          ],
-        });
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+      //Représentation sur un mois//
+      initialView: 'dayGridMonth',
+      events: [
+        //objets pour peupler les cellules//
+        {
+          title: 'Jean Euze',
+          start: '2023-08-15T08:00:00',
+          end: '2023-08-15T09:30:00',
+        },
+      ],
+    });
 
-      calendar.render();
-    }
+    calendar.render();
+  }
 }
 
 export { page };
