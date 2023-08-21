27b71959330d4fd84c4b60efef173c762579423a
Kevin R <krous74@gmail.com>
2023-08-21 14:10:13 +0200

    nième message

--- Page/Dashboard.php
+++ Page/Dashboard.php
@@ -5,17 +5,16 @@
   alt="image d'une personne les bras ouverts"
   srcset="./img/dashboard/happy-body-desktop.svg 970w, ./img/dashboard/happy-body.svg 320w"
   sizes="(min-width: 320px) and (max-width: 979px) 100vw, (min-width: 980px) and (max-width: 1920px) 970px, 100vw"
->
+  >
     <div class="blur">
       <div id="calendar"></div>
     </div>
   </div>
 
 <?php
-// <!-- 
+//   -- Le calendrier reste le même, il est simplement peuplé différemment en fonction des infos de la base de donnée.
 //   -- Ajouter un selecteur pour nourrir le tableu en fonction des rôles utilisateur
 //   -- Si l'utilisateur est nounou, le calendrier est appellé "agenda". Le sélecteur permettra donc de visualiser les disponibilités des emplacements par nounou
-
 //   -- Si l'utilisateur est parent :
-// -->
+
 ?>
