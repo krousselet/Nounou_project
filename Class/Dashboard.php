6ee999518a9322837c5df5e8baacf88bcced22c6
Kevin R <krous74@gmail.com>
2023-08-22 14:24:14 +0200

    None

--- Class/Dashboard.php
+++ Class/Dashboard.php
@@ -7,7 +7,9 @@ class Dashboard extends Bdd{
     }
 
     private function Parent() {
-        echo "<button class='btn_d_addChild'>Ajouter un enfant</button>";
+        echo "<div class='role-container'>";
+        echo "<button class='btn_d_addChild btn btn-custom-color'>Ajouter un enfant</button>";
+        echo "</div>";
     }
 
     private function Child() {
@@ -15,24 +17,27 @@ class Dashboard extends Bdd{
     }
 
     private function Nounou() {
-        echo "<button class='btn_d_Charge'>Enfant a charge</button>";
-        echo "<button class='btn_d_calendar'>Agenda</button>";
-        echo "<button class='btn_d_addChild'>Ajouté un enfant</button>";
-        echo "<button class='btn_d_Facture'>Facture</button>";
+        echo "<div class='role-container'>";
+        echo "<button class='btn_d_Charge btn btn-custom-color'>Enfant a charge</button>";
+        echo "<button class='btn_d_calendar btn btn-custom-color'>Agenda</button>";
+        echo "<button class='btn_d_addChild btn btn-custom-color'>Ajouté un enfant</button>";
+        echo "<button class='btn_d_Facture btn btn-custom-color'>Facture</button>";
+        echo "</div>";
     }
 
     public function Display(){
         $session = new Session();
         $nom = $session->GetNom();
         $prenom = $session->GetPrenom();
-        echo "<div class='Dashboard'>";
-        echo "<h2>Bienvenue $nom $prenom <h2>";
+        echo "<div class='Dashboard'>
+        <h2>Bienvenue $nom $prenom </h2>
+        </div>";
         if($session->GetRole() == "nounou"){
             $this->Nounou();
         }else{
             $this->Parent();
         }
-        echo "</div>";
+        
     }
 
 }
--- Page/Dashboard.php
+++ Page/Dashboard.php
@@ -1,13 +1,7 @@
 <?php include __DIR__.'/../partsPhp/loader.php';?>
 
 <div class="happy-body-container">
-  <img
-  src="./img/dashboard/happy-body.svg"
-  class="moving-rect happy-body"
-  alt="image d'une personne les bras ouverts"
-  srcset="./img/dashboard/happy-body-desktop.svg 970w, ./img/dashboard/happy-body.svg 320w"
-  sizes="(min-width: 320px) and (max-width: 979px) 100vw, (min-width: 980px) and (max-width: 1920px) 970px, 100vw"
-  >
+  
     <div class="blur">
       <?php 
       
