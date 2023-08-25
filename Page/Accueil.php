32bb9872cc18bc5b2a0f4a95393d685629d941e6
lambert-j <jordan.lambert.pro@gmail.com>
2023-08-25 15:38:45 +0200

    mobile

--- Page/Accueil.php
+++ Page/Accueil.php
@@ -1,4 +1,4 @@
-<div id="bienvenu" class="text-center mt-4 slide-bottom">Bienvenu</div>
+<div id="bienvenu" class="text-center mt-4 slide-bottom col-12">Bienvenu</div>
 
 <div
   class="container d-flex flex-column justify-content-center align-items-center accueil-welcome-box"
@@ -6,39 +6,39 @@
 
 <!-- Bloc Image Texte Welcome -->
   <div
-    class="accueil-text-welcome accueil-text-box d-flex justify-content-center col-12 mt-2 mb-5"
+    class="accueil-text-welcome accueil-text-box d-flex justify-content-center mt-2 mb-4 flex-wrap"
   >
-    <div class="accueil-text-welcome-text accueil-triple-text card">
+    <div class="accueil-text-welcome-text accueil-triple-text card col-lg-5 col-12">
       Qu'il s'agisse de simplifier la vie des parents occupés ou d'offrir aux
       nounous les outils pour réussir, <b>Nounou Projet X</b> est le partenaire
       idéal pour une garde d'enfants efficace et bienveillante. Rejoignez-nous
       dès aujourd'hui et découvrez une nouvelle façon de gérer la garde
       d'enfants !
     </div>
-    <img class="accueil-img col-6 shadow-pop-tr-left " src="../img/couch-family.jpg" alt="" />
+    <img class="accueil-img col-lg-6 col-12 shadow-pop-tr-left " src="../img/couch-family.jpg" alt="" />
   </div>
 
 
 <!-- Bloc Image + Parent -->
-  <div class="accueil-text-box d-flex justify-content-center align-items-center flex-row-reverse col-12 mt-5">
-    <div class="accueil-text-main-text">Pour les parents</div>
+  <div class="accueil-text-box accueil-parent-box d-flex justify-content-center align-items-center flex-row-reverse flex-wrap  mt-4">
+    <div class="accueil-text-main-text col-lg-4 col-12">Pour les parents</div>
 
-    <img class="accueil-img col-6 shadow-pop-tl-right" src="../img/nice-house.jpg" alt="" />
+    <img class="accueil-img  col-lg-6 col-12 shadow-pop-tl-right" src="../img/nice-house.jpg" alt="" />
   </div>
 
 <!-- Bloc 3 Box Texte Parent -->
-  <div class="col-12 d-flex text-center align-items-center mt-5 mb-5 accueil-triple-box ">
-    <div class="accueil-triple-text card">
+  <div class="col-12 d-flex text-center align-items-center mb-4 accueil-triple-box flex-wrap">
+    <div class="accueil-triple-text card col-lg-3 col-12 mt-2">
       Simplifiez la parentalité avec notre plateforme : besoins de garde
       facilités, nounous compétentes à proximité, gestion fluide et suivi
       transparent pour le bien-être de vos enfants
     </div>
-    <div class="accueil-triple-text card">
+    <div class="accueil-triple-text card col-lg-3 col-12 mt-2">
       Confiez vos trésors en toute sérénité grâce à notre réseau de nounous
       fiables. Sélectionnez la personne idéale pour veiller sur vos enfants, en
       parcourant profils, avis et disponibilités
     </div>
-    <div class="accueil-triple-text card">
+    <div class="accueil-triple-text card col-lg-3 col-12 mt-2">
       Optimisez la garde d'enfants en ligne : planification, notifications et
       gestion simplifiée grâce à notre plateforme intuitive. Fini les tracas,
       place à une gestion efficace !
@@ -48,26 +48,26 @@
 
 
 <!-- Bloc Image/ Titre Nounou -->
-  <div class=" col-12 mt-5 d-flex justify-content-center align-items-center accueil-text-box  ">
-    <div class="accueil-text-main-text">Pour les nounous</div>
+  <div class="mt-4 d-flex justify-content-center align-items-center accueil-text-box flex-wrap">
+    <div class="accueil-text-main-text col-lg-4 col-12">Pour les nounous</div>
 
-    <img class="accueil-img col-6 shadow-pop-tr-left" src="../img/kids-tree.jpg" alt="" />
+    <img class="accueil-img  col-lg-6 col-12 shadow-pop-tr-left" src="../img/kids-tree.jpg" alt="" />
   </div>
 
 <!-- Bloc 3 Texte Nounou -->
-<div class="col-12 d-flex text-center align-items-center mt-5 mb-5 accueil-triple-box ">
+<div class="d-flex text-center align-items-center mb-4 accueil-triple-box flex-wrap">
 
-    <div class="accueil-triple-text card ">
+    <div class="accueil-triple-text card  col-lg-3 col-12 mt-2">
       Rejoignez une communauté valorisant votre travail en tant que nounou.
       Accédez aux meilleures opportunités, gérez votre emploi du temps,
       établissez des liens avec les familles et simplifiez votre facturation
     </div>
-    <div class="accueil-triple-text card">
+    <div class="accueil-triple-text card col-lg-3 col-12  mt-2">
       Prenez les rênes de votre carrière de nounou : contrôlez horaires,
       missions et relations avec les parents grâce à notre interface conviviale,
       pour des expériences de garde exceptionnelles
     </div>
-    <div class="accueil-triple-text card">
+    <div class="accueil-triple-text card col-lg-3 col-12  mt-2">
       Gérez, gardez, gagnez : notre plateforme simplifie la gestion, la
       communication et les factures pour que vous puissiez vous concentrer sur
       une garde attentionnée et de qualité
