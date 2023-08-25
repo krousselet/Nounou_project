4fb679791bbd9f1ac0771740d76c407e499a051c
Kevin R <krous74@gmail.com>
2023-08-25 15:35:03 +0200

    path drawing

--- Page/Accueil.php
+++ Page/Accueil.php
@@ -1,111 +1,21 @@
-<div id="bienvenu" class="text-center mt-4 slide-bottom">Bienvenu</div>
+<div id="bienvenu" class="text-center mt-4">Bienvenu</div>
 
-<div
-  class="container d-flex flex-column justify-content-center align-items-center accueil-welcome-box"
->
+<div class=" container d-flex btns-accueil align-items-center justify-content-center flex-wrap">
 
-<!-- Bloc Image Texte Welcome -->
-  <div
-    class="accueil-text-welcome accueil-text-box d-flex justify-content-center col-12 mt-2 mb-5"
-  >
-    <div class="accueil-text-welcome-text accueil-triple-text card">
-      Qu'il s'agisse de simplifier la vie des parents occupés ou d'offrir aux
-      nounous les outils pour réussir, <b>Nounou Projet X</b> est le partenaire
-      idéal pour une garde d'enfants efficace et bienveillante. Rejoignez-nous
-      dès aujourd'hui et découvrez une nouvelle façon de gérer la garde
-      d'enfants !
-    </div>
-    <img class="accueil-img col-6 shadow-pop-tr-left " src="../img/couch-family.jpg" alt="" />
-  </div>
-
-
-<!-- Bloc Image + Parent -->
-  <div class="accueil-text-box d-flex justify-content-center align-items-center flex-row-reverse col-12 mt-5">
-    <div class="accueil-text-main-text">Pour les parents</div>
-
-    <img class="accueil-img col-6 shadow-pop-tl-right" src="../img/nice-house.jpg" alt="" />
-  </div>
-
-<!-- Bloc 3 Box Texte Parent -->
-  <div class="col-12 d-flex text-center align-items-center mt-5 mb-5 accueil-triple-box ">
-    <div class="accueil-triple-text card">
-      Simplifiez la parentalité avec notre plateforme : besoins de garde
-      facilités, nounous compétentes à proximité, gestion fluide et suivi
-      transparent pour le bien-être de vos enfants
-    </div>
-    <div class="accueil-triple-text card">
-      Confiez vos trésors en toute sérénité grâce à notre réseau de nounous
-      fiables. Sélectionnez la personne idéale pour veiller sur vos enfants, en
-      parcourant profils, avis et disponibilités
-    </div>
-    <div class="accueil-triple-text card">
-      Optimisez la garde d'enfants en ligne : planification, notifications et
-      gestion simplifiée grâce à notre plateforme intuitive. Fini les tracas,
-      place à une gestion efficace !
-    </div>
-  </div>
-
-
-
-<!-- Bloc Image/ Titre Nounou -->
-  <div class=" col-12 mt-5 d-flex justify-content-center align-items-center accueil-text-box  ">
-    <div class="accueil-text-main-text">Pour les nounous</div>
-
-    <img class="accueil-img col-6 shadow-pop-tr-left" src="../img/kids-tree.jpg" alt="" />
-  </div>
-
-<!-- Bloc 3 Texte Nounou -->
-<div class="col-12 d-flex text-center align-items-center mt-5 mb-5 accueil-triple-box ">
-
-    <div class="accueil-triple-text card ">
-      Rejoignez une communauté valorisant votre travail en tant que nounou.
-      Accédez aux meilleures opportunités, gérez votre emploi du temps,
-      établissez des liens avec les familles et simplifiez votre facturation
-    </div>
-    <div class="accueil-triple-text card">
-      Prenez les rênes de votre carrière de nounou : contrôlez horaires,
-      missions et relations avec les parents grâce à notre interface conviviale,
-      pour des expériences de garde exceptionnelles
-    </div>
-    <div class="accueil-triple-text card">
-      Gérez, gardez, gagnez : notre plateforme simplifie la gestion, la
-      communication et les factures pour que vous puissiez vous concentrer sur
-      une garde attentionnée et de qualité
-    </div>
-
-
-</div>
-
-</div>
-
-<!-- Deux Cards de Connexions/Enregistrer -->
-<div
-  class="container d-flex btns-accueil align-items-center justify-content-center flex-wrap"
->
-  <div class="card form-log-reg" style="width: 24rem">
-    <img src="./img/parent-kids.png" class="card-img-top" alt="..." />
-    <div
-      class="card-body d-flex flex-column justify-content-center align-items-center"
-    >
+  <div class="card form-log-reg  " style="width: 24rem;">
+    <img src="./img/parent-kids.png" class="card-img-top" alt="...">
+    <div class="card-body d-flex flex-column justify-content-center align-items-center">
       <h5 class="card-title mb-4">Vous possédez déjà un compte ?</h5>
-      <a href="#" class="btn btn-custom-color btn-accueil-connect"
-        >Se connecter</a
-      >
+      <a href="#" class="btn btn-custom-color ">Se connecter</a>
     </div>
   </div>
 
-  <div
-    class="card form-log-reg justify-content-center align-items-center"
-    style="width: 24rem"
-  >
-    <img src="./img/parent.png" class="card-img-top" alt="..." />
-    <div
-      class="card-body d-flex flex-column justify-content-center align-items-center"
-    >
+  <div class="card form-log-reg   justify-content-center align-items-center" style="width: 24rem;">
+    <img src="./img/parent.png" class="card-img-top" alt="...">
+    <div class="card-body d-flex flex-column justify-content-center align-items-center">
       <h5 class="card-title mb-4">Pas encore de compte ?</h5>
-      <a href="#" class="btn btn-custom-color btn-accueil-register"
-        >Créer un compte</a
-      >
+      <a href="#" class="btn btn-custom-color ">Créer un compte</a>
     </div>
   </div>
+
 </div>
--- partsPhp/header.php
+++ partsPhp/header.php
@@ -1,90 +1,92 @@
-<?php 
-include __DIR__.'/loader.php';
+<?php
+include __DIR__ . '/loader.php';
 
 $session = new Session(); ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <title>I'm IN</title>
-    <!-- <base href="/Nounou_project"> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./style/style.css" />
-    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.1/main.css" rel="stylesheet">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap" rel="stylesheet">
+  <meta name="description" content="">
+  <title>I'm IN</title>
+  <!-- <base href="/Nounou_project"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./style/style.css" />
+  <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.1/main.css" rel="stylesheet">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
-<script>
-    <?php if($session->isLogin()){
-        echo "type = '".$session->GetRole()."'";
-    }?>
-</script>
-<nav class="navbar navbar-expand-lg bg-primary text-light">
+  <script>
+    <?php if ($session->isLogin()) {
+      echo "type = '" . $session->GetRole() . "'";
+    } ?>
+  </script>
+  <nav class="navbar navbar-expand-lg bg-primary text-light">
     <div class="container-fluid">
-        <!-- Logo à gauche -->
-        <a class="navbar-brand text-light" href="#"><img src="./img/Logo.png" alt="Logo" height="32" loading="Nounou Project X"> Nounou Project X</a>
+      <!-- Logo à gauche -->
+      <a class="navbar-brand text-light" href="#"><img src="./img/Logo.png" alt="Logo" height="32" loading="Nounou Project X"> Nounou Project X</a>
 
-        <!-- Bouton pour les écrans de petite taille -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+      <!-- Bouton pour les écrans de petite taille -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <!-- Menu à droite -->
-        <div class="collapse navbar-collapse justify-content-end" id="navbarMenu">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link text-light btn_home" href="">Accueil</a>
-                </li>
-                <?php if($session->isLogin()){?>
-                    <li class="nav-item">
-                        <a class="nav-link text-light btn_dash">Dashboard</a>
-                    </li>
-                <?php }?>
-                <!-- Dropdown pour le compte -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-light" href="#s" id="accountDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Compte
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end bg-secondary" aria-labelledby="accountDropdown">
-                        <?php if($session->isLogin()) { ?>
-                        <li><a class="dropdown-item btn_profil text-light" href="#">Profile</a></li>
-                        <li><a class="dropdown-item btn_logout text-light" href="#">Déconnexion</a></li>
-                        <?php }else{?>
-                        <li><a class="dropdown-item btn_login text-light" href="#">Connexion</a></li>
-                        <li><a class="dropdown-item btn_regis text-light" href="#">Inscription</a></li>
-                        <?php }?>
-                    </ul>
-                </li>
+      <!-- Menu à droite -->
+      <div class="collapse navbar-collapse justify-content-end" id="navbarMenu">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-light btn_home" href="">Accueil</a>
+          </li>
+          <?php if ($session->isLogin()) { ?>
+            <li class="nav-item">
+              <a class="nav-link text-light btn_dash">Dashboard</a>
+            </li>
+          <?php } ?>
+          <!-- Dropdown pour le compte -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-light" href="#s" id="accountDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Compte
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end bg-secondary" aria-labelledby="accountDropdown">
+              <?php if ($session->isLogin()) { ?>
+                <li><a class="dropdown-item btn_profil text-light" href="#">Profil</a></li>
+                <li><a class="dropdown-item btn_logout text-light" href="#">Déconnexion</a></li>
+              <?php } else { ?>
+                <li><a class="dropdown-item btn_login text-light" href="#">Connexion</a></li>
+                <li><a class="dropdown-item btn_regis text-light" href="#">Inscription</a></li>
+              <?php } ?>
             </ul>
-        </div>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
 
-<!-- Button trigger modal -->
-<!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  <!-- Button trigger modal -->
+  <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
   Launch static backdrop modal
 </button> -->
 
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Ttitre de la modal</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Texte à l'intérieur de la modal
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-custom-color" data-bs-dismiss="modal">Close</button>
+  <!-- Modal -->
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Ttitre de la modal</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Texte à l'intérieur de la modal
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-custom-color" data-bs-dismiss="modal">Close</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
